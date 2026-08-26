ccd0a17292a8ff7190048f44e9c601b08d17ab36
unknown <ITMDC@MDC-FT9YYB4.apex.local>
2026-08-26 14:10:21 -0600

    disable lazy.nvim partial clones (fix copilot.lua on IPv6-broken hosts)

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
+  git = { filter = false }, -- disable partial clones (broken on IPv6-impaired hosts)
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
